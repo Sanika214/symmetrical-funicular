b62aff6a3c0174ae136b31c31f54aea2da4f73ee
Sanika214 <sanikachakk@gmail.com>
2025-11-03 14:12:32 -0500

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<script>
+var x = "Today you are you"
+var reverse = "";
+for(i = x.length - 1; i >= 0; i--){
+  reverse+= x.charAt(i);
+}
+
+console.log(x);
+console.log(reverse);
+</script>
+<html>
 
